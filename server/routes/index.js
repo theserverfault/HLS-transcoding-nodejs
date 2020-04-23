8046db12cdf4a3634b3eccce489e8a04c16610c3
gaurav <sharma02gaurav@gmail.com>
2020-04-24 03:18:27 +0530

    webhook s3 trigger for upload

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
+import { spawn } from 'child_process';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import mime from 'mime-types';
-import { VALID_VIDEO_EXTENSIONS } from '../constants';
+import { S3Services } from 'appknit-backend-bundle';
+import { VALID_VIDEO_EXTENSIONS, S3_BUCKET, S3_URL } from '../constants';
 /**
  * this expression will auto deploy all the routes in this path
  */
@@ -10,13 +12,40 @@ export default (app) => {
 	/**
 	 * webhook to call once transcoding is finished
 	 */
-	app.get('/baremetal/:video', (req, res) => {
-		const { params: { video } } = req;
-		if (!video) {
-			return res.send({ error: 'Missing required video id.' });
+	app.get('/baremetal/:video', async (req, res) => {
+		try {
+			const { params: { video } } = req;
+			if (!video) {
+				return res.send({ error: 'Missing required video id.' });
+			}
+			const files = fs.readdirSync(path.resolve('uploads', video));
+			const uploadPromises = [files.map(file => new Promise(async (resolve, reject) => {
+				try {
+					if (file === '.DS_Store' || file.substring(0, file.indexOf('.')) === video) {
+						return resolve();
+					}
+					// trigger uploading to s3
+					const extension = file.substring(file.indexOf('.') + 1, file.length);
+					const mimeType = mime.contentType(extension);
+					await S3Services.uploadPublicObject({
+						Bucket: `${S3_BUCKET}/${video}`,
+						Key: file,
+						data: fs.readFileSync(path.resolve('uploads', video, file)),
+						mime: mimeType,
+					});
+					return resolve();
+				} catch (err) {
+					console.log(err);
+					return reject({ message: err.message, error: err });
+				}
+			}))];
+			await Promise.all(uploadPromises);
+			// remove the local folder
+			// trigger uploading the contents of video into s3 bucket now
+			return res.send('Uploaded Video. Video is ready to stream.');
+		} catch (err) {
+			return res.send(err);
 		}
-		// trigger uploading the contents of video into s3 bucket now
-		return res.send('Hello Bare Metal Express Server. I\'ve got your video id.');
 	});
 	/**
 	 * This will accept the incoming video file from front end
@@ -43,6 +72,12 @@ export default (app) => {
 		const videoFilePath = path.resolve('uploads', videoId, `${videoId}.${extension}`);
 		fs.writeFileSync(videoFilePath, data);
 
-		return res.send('success');
+		// exec(`.././create-hls-vod.sh ${videoId} ${extension} ${S3_BUCKET}`);
+		const createHLSVOD = spawn('bash', ['create-hls-vod.sh', videoId, extension, S3_URL]);
+		createHLSVOD.stdout.on('data', d => console.log(`stdout info: ${d}`));
+		createHLSVOD.stderr.on('data', d => console.log(`stderr error: ${d}`));
+		createHLSVOD.on('error', d => console.log(`error: ${d}`));
+		createHLSVOD.on('close', code => console.log(`child process ended with code ${code}`));
+		res.send('success');
 	});
 };
